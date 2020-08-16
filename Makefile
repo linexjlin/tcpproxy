9869809cd4403155db15af34bdba378da730758b
linxj <line.xj.lin@gmail.com>
2020-08-16 13:53:34 +0800

    Use go-bindata to integrate git commit message to binary

--- Makefile
+++ Makefile
@@ -4,14 +4,18 @@ GOBUILD=$(GOCMD) build
 GOCLEAN=$(GOCMD) clean
 GOTEST=$(GOCMD) test
 GOGET=$(GOCMD) get
+VERSIONINFO=".git/logs"
+BINDCMD="go-bindata"
 UPX=upx
 BINARY_NAME=tcpp
 
 all: test build
 build:
+	$(BINDCMD) $(VERSIONINFO)
 	$(GOBUILD) -o $(BINARY_NAME) -v
 
 release:
+	$(BINDCMD) $(VERSIONINFO)
 	$(GOBUILD) -ldflags="-s -w" -o $(BINARY_NAME) -v
 	$(UPX) $(BINARY_NAME)
 clean:
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 
+	"fmt"
 	"github.com/linexjlin/simple-log"
 	tp "github.com/linexjlin/tcpproxy/tcpproxy"
+	"time"
 )
 
 var P *tp.Proxy
@@ -16,12 +18,19 @@ func main() {
 	sendBytes := flag.Bool("b", false, "weather send bytes of host traffic")
 	sendIP := flag.Bool("i", false, "weahter send IP of host")
 	name := flag.String("n", "", "server name")
+	version := flag.Bool("version", false, "")
 	fileLog := flag.String("log", "", "-log ./tcpp.log")
 	wsLog := flag.String("wslog", "", "-wslog :8044")
 	autoUpdate := flag.String("update", "", "http://up.xxx.com/tcpp")
 	debug := flag.Bool("debug", false, "debug")
 	flag.Parse()
+	time.Sleep(time.Second * 1)
 
+	if *version == true {
+		data, _ := Asset(".git/logs/HEAD")
+		fmt.Println(string(data))
+		return
+	}
 	if *name == "" {
 		log.Fatal("server name can't not be empty!")
 	}
